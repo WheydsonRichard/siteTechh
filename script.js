827bab4857a1429177030e3f1cec4e1eacad9807
Wheydson richard <126586552+Wheydson@users.noreply.github.com>
2023-09-15 23:10:55 -0300

    feito ajuste de nova layout

---
+++ script.js
@@ -0,0 +1,71 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var zapImage = document.getElementById('whatsapp');
+
+    zapImage.addEventListener("click", function () {
+        // Altere o link do WhatsApp conforme necessário
+        var whatsappLink = "https://api.whatsapp.com/send?phone=5585996473206";
+        
+        // Abre o link do WhatsApp em uma nova janela ou guia
+        window.open(whatsappLink, "_blank");
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    var instImage = document.getElementById('instagram');
+
+    instImage.addEventListener("click", function () {
+        // Altere o link do WhatsApp conforme necessário
+        var instagramLink = "https://www.instagram.com/timetechsoftware";
+        
+        // Abre o link do WhatsApp em uma nova janela ou guia
+        window.open(instagramLink, "_blank");
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    var gitImage = document.getElementById('git');
+
+    gitImage.addEventListener("click", function () {
+        // Altere o link do WhatsApp conforme necessário
+        var gitLink = "https://github.com/WheydsonRichard";
+        
+        // Abre o link do WhatsApp em uma nova janela ou guia
+        window.open(gitLink, "_blank");
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    var tiktokImage = document.getElementById('tiktok');
+
+    tiktokImage.addEventListener("click", function () {
+        // Altere o link do WhatsApp conforme necessário
+        var tiktokLink = "https://tiktok.com/";
+        
+        // Abre o link do WhatsApp em uma nova janela ou guia
+        window.open(tiktokLink, "_blank");
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    var figImage = document.getElementById('fig');
+
+    figImage.addEventListener("click", function () {
+        // Altere o link do WhatsApp conforme necessário
+        var figLink = "https://api.whatsapp.com/send?phone=5585996473206";
+        
+        // Abre o link do WhatsApp em uma nova janela ou guia
+        window.open(figLink, "_blank");
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    var fig1Image = document.getElementById('fig1');
+
+    fig1Image.addEventListener("click", function () {
+        // Altere o link do WhatsApp conforme necessário
+        var fig1Link = "https://www.instagram.com/timetechsoftware";
+        
+        // Abre o link do WhatsApp em uma nova janela ou guia
+        window.open(fig1Link, "_blank");
+    });
+});
